b9717e8592afb2e2774d046c9769ff53434843cf
xairry <xairry7@gmail.com>
2017-11-18 00:29:16 +0800

    Delete createdir.py

--- createdir.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-import os
-for i in range(1005,1076):
-   # print i
-    File_Path = os.getcwd() +'\\'+ str(i)
-         #获取到当前文件的目录，并检查是否有report文件夹，如果不存在则自动新建report文件
-    print (File_Path)
-    if not os.path.exists(File_Path):
-        os.makedirs(File_Path)
