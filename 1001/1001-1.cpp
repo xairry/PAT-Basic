136610541c8aacb2cff0451d6754dc7548ff7a9e
xyy <xairry7@gmail.com>
2017-11-17 21:07:26 +0800

    for my first time

---
+++ 1001/1001-1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+ int n;
+ int count=0;
+ cin>>n;
+ while(n!=1){
+   if(n%2==0){
+     n=n/2;
+     count++;
+   }
+  else{
+	 n=(3*n+1)/2;
+	 count++;
+  }
+}
+ cout<<count<<endl;
+}
