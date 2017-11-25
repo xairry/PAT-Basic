46b6afdc1b5b0b5cd9f130ec84fdd279b5460d45
wenjie1991 <sunwjie@gmail.com>
2017-11-25 13:36:20 +0800

    update 1071; correct the input, output format.

--- 1071/1071-0.cpp
+++ 1071/1071-0.cpp
@@ -4,24 +4,23 @@
 using namespace std;
 
 int main() {
-    int T, K, n1, b, t, n2, sum;
-    sum = 100;
+    int T, K, n1, b, t, n2;
     cin>>T>>K;
 
     for (int i=0; i<K; i++) {
         cin>>n1>>b>>t>>n2;
 
-        if (t > sum) {
-            cout<<"Not enough tokens. Total = "<<sum<<"."<<endl;
+        if (t > T) {
+            cout<<"Not enough tokens.  Total = "<<T<<"."<<endl;
         } else if ((n1 > n2 & b == 0) || (n1 < n2 & b == 1)) {
             // win
-            sum += t;
-            cout<<"Win "<<t<<"! "<<"Total = "<<sum<<"."<<endl;
+            T += t;
+            cout<<"Win "<<t<<"!  Total = "<<T<<"."<<endl;
         } else {
             // lose
-            sum = sum - t;
-            cout<<"Loss "<<t<<". "<<"Total = "<<sum<<"."<<endl;
-            if (sum <= 0) {
+            T = T - t;
+            cout<<"Lose "<<t<<".  "<<"Total = "<<T<<"."<<endl;
+            if (T <= 0) {
                 cout<<"Game Over.";
                 break;
             }
