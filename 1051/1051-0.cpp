2c0e02e9a3e4f80de1571cee66444292b1c902df
wenjie1991 <sunwjie@gmail.com>
2017-11-25 13:12:48 +0800

    1051 correction

--- 1051/1051-0.cpp
+++ 1051/1051-0.cpp
@@ -5,6 +5,8 @@
 
 using namespace std;
 
+double round2(double);
+
 int main() {
     double r1, r2, p1, p2;
     cin>>r1>>p1>>r2>>p2;
@@ -14,12 +16,19 @@ int main() {
     real = r1 * r2 * (cos(p1) * cos(p2) - sin(p1) * sin(p2));
     i = r1 * r2 * (sin(p1) * cos(p2) + sin(p2) * cos(p1));
 
-    real = (abs(real) + 0.005) * real / abs(real);
-    i = (abs(i) + 0.005) * i / abs(i);
-
-//    cout<<fixed<<setprecision(2);
-//    cout<<real<<i<<"i"<<endl;
-    printf("%.2lf%.2lfi", real, i);
+    if (real<0 && real > -0.001) {
+        printf("0.00");	//real<0且大于0.001的时候四舍五入是0，要单独处理
+    } else {
+        printf("%.2lf",real);
+    }
+    if (i>=0) {
+        printf("%+.2lfi", i);
+    } else if (i<0 && i > -0.001) {
+        printf("+0.00i");//i同理
+    } else {
+        printf("%.2lfi",i);
+    }
 
     return 0;
 }
+
