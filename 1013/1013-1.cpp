de07cb4dba7dc9191c5550a45daf1954f3e02c30
xyy <xairry7@gmail.com>
2017-12-08 23:31:25 +0800

    ha

--- 1013/1013-1.cpp
+++ 1013/1013-1.cpp
@@ -1,29 +1,9 @@
 #include <cstdio>
 #include <set>
 using namespace std;
-int getFriendNum(int num) {
-    int sum = 0;
-    while(num != 0) {
-        sum += num % 10;
-        num /= 10;
-    }
-    return sum;
-}
-int main() {
-    set<int> s;
-    int n, num;
-    scanf("%d", &n);
-    for(int i = 0; i < n; i++) {
-        scanf("%d", &num);
-        s.insert(getFriendNum(num));
-    }
-    printf("%d\n", s.size());
-    set<int>::iterator it;
-    for(it=s.begin();it!=s.end();it++)
-    //for(auto it = s.begin(); it != s.end(); it++) 
-	{
-        if(it != s.begin()) printf(" ");
-        printf("%d", *it);
-    }
-    return 0;
+typedef list<int> a;
+typedef list<char> b;
+
+void main(){
+		
 }
