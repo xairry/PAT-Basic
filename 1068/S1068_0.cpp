09ebde477359a4ba5134183a8d99086279dfea56
wenjie1991 <sunwjie@gmail.com>
2018-03-19 14:51:20 +0800

    update ans

--- 1068/S1068_0.cpp
+++ 1068/S1068_0.cpp
@@ -5,53 +5,51 @@
 
 using namespace std;
 
+const int maxn = 1<<25;
+
 int main() {
 
     int M, N, count = 0;
     long long threshold;
     scanf("%d %d %lld", &M, &N, &threshold);
 
-    long long ** array = new long long*[N];
-    for (int i=0; i<N; i++) {
-        array[i] = new long long[M];
+    long long ** array = new long long*[N+2];
+    for (int i=0; i<N+2; i++) {
+        array[i] = new long long[M+2];
+        memset(array[i], 0, M+2);
     }
 
-    for (int i=0; i<N; i++) {
-        for (int j=0; j<M; j++) {
+    long long * hash = new long long[maxn];
+    memset(hash, 0, maxn);
+
+    for (int i=1; i<N+1; i++) {
+        for (int j=1; j<M+1; j++) {
             scanf("%lld", &array[i][j]);
+            hash[array[i][j]]++;
         }
     }
 
     int x, y;
     long long value;
-    for (int i=1; i<N-1; i++) {
-        for (int j=1; j<M-1; j++) {
-
+    for (int i=1; i<N+1; i++) {
+        for (int j=1; j<M+1; j++) {
             if (
                     (
-                     ((array[i][j] - array[i-1][j-1]) >= threshold) &&
-                     ((array[i][j] - array[i-1][j])   >= threshold) &&
-                     ((array[i][j] - array[i-1][j+1]) >= threshold) &&
-                     ((array[i][j] - array[i][j-1])   >= threshold) &&
-                     ((array[i][j] - array[i][j+1])   >= threshold) &&
-                     ((array[i][j] - array[i+1][j-1]) >= threshold) &&
-                     ((array[i][j] - array[i+1][j])   >= threshold) &&
-                     ((array[i][j] - array[i+1][j+1]) >= threshold)
-                    ) || 
-                    (
-                     ((array[i][j] - array[i-1][j-1]) <= -threshold) &&
-                     ((array[i][j] - array[i-1][j])   <= -threshold) &&
-                     ((array[i][j] - array[i-1][j+1]) <= -threshold) &&
-                     ((array[i][j] - array[i][j-1])   <= -threshold) &&
-                     ((array[i][j] - array[i][j+1])   <= -threshold) &&
-                     ((array[i][j] - array[i+1][j-1]) <= -threshold) &&
-                     ((array[i][j] - array[i+1][j])   <= -threshold) &&
-                     ((array[i][j] - array[i+1][j+1]) <= -threshold)
+                     (abs(array[i][j] - array[i-1][j-1]) > threshold) &&
+                     (abs(array[i][j] - array[i-1][j])   > threshold) &&
+                     (abs(array[i][j] - array[i-1][j+1]) > threshold) &&
+                     (abs(array[i][j] - array[i][j-1])   > threshold) &&
+                     (abs(array[i][j] - array[i][j+1])   > threshold) &&
+                     (abs(array[i][j] - array[i+1][j-1]) > threshold) &&
+                     (abs(array[i][j] - array[i+1][j])   > threshold) &&
+                     (abs(array[i][j] - array[i+1][j+1]) > threshold) &&
+                     hash[array[i][j]] == 1
                     )
-            )  {
+                    )  {
 
-                x = i+1;
-                y = j+1;
+//                cout<<hash[array[i][j]]<<"_"<<array[i][j]<<endl;
+                x = i;
+                y = j;
                 value = array[i][j];
                 count++;
             }
@@ -77,4 +75,6 @@ int main() {
 //   If the item greater than the 8 neibor beyoung the threshold count++
 //   If another item greater than the 8 neibor -> output unnique
 //   If count < 1 -> output no exist
+// Check point 3 -> abs 
+// Check point 5 -> the element around
 
